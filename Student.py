d9e48df8dc42fe4b9abd127be7530f009880e3b1
ajrace <143133603+ajrace@users.noreply.github.com>
2024-03-24 22:30:44 -0500

    Updated student,py

--- Student.py
+++ Student.py
@@ -32,18 +32,28 @@ print("UDP ports <ddata> received!!" %ddata)
 portnum = ddata.split(",")
 udp_rob_port = int(portnum[0])
 udp_stud_port = int(portnum[1])
+
 s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-num = 7
-s3.sendto(num.encode(),localhost,udp_rob_port)
+num = random.randint(6,9)
+s3.sendto(num.encode(),(localhost,udp_rob_port))
 print("Sending %d using port %d" % (num,udp_rob_port))
 
-s4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s4.bind(localhost,udp_stud_port)
-receiveddata = s4.recvfrom(1024)
+receiveddata, addr = s3.recvfrom(1024)
+recieveddata = data.decode()
 print("Received %d from port %d" %(receiveddata,udp_stud_port))
 
+print("\nSending UDP packets: ")
+for i in range(0,5):
+    s3.sendto(recieveddata,(localhost,udp_rob_port))
+    time.sleep(1)
+    print("Packet %d sent!" %(i+1))
 
+print("Sent!")
 
+s1.close()
+s2.close()
+s3.close()
+exit()
 
 
 
